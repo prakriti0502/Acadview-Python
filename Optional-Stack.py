5768f084d1fb688a6b3b8aaabaf3178e0edd8024
prakriti0502 <Prakriti.sagar@icloud.com>
2018-08-17 00:18:50 +0530

    ,

--- Optional-Stack.py
+++ Optional-Stack.py
@@ -1,8 +1,18 @@
 n=int(input("Enter number of elements in stack\n"))
-stack=[]
+l1=[]
 print("Enter elements")
 for i in range(n):
     a=int(input())
-    stack.append(a)
-print("Stack is ",stack)
-print("After popping " , stack.pop() , "\nStack is: " , stack)
+    l1.append(a)
+choice=int(input("To implement using queue, press 1, and to implement using stack, press 2"))
+if choice==1:
+    print("Queue is : ",l1)
+    ad=int(input("Enter a new element"))
+    l1.append(ad)
+    print("Queue after appending " , ad , " is " , l1)
+    print("Queue after popping ", l1.pop(0) ," is: " ,l1) 
+elif choice==2:
+    print("Stack is ",l1)
+    ad=int(input("Enter a new element"))
+    l1.append(ad)
+    print("After popping " , l1.pop() , "\nStack is: " , l1)
