12c4d0ede9e747aba8a699a221f21bb27015a0e0
prakriti0502 <Prakriti.sagar@icloud.com>
2018-08-16 22:33:41 +0530

    Assignment 2 done

---
+++ Assigment-2.py
@@ -0,0 +1,30 @@
+#Question 1
+a='Hello Acadview'
+print(a)
+
+#Question 2
+str1='Acad'
+str2='View'
+str3=str1+str2
+print(str3)
+
+#Question 3
+x=int(input("Enter value in first variable"))
+y=int(input("Enter value in second variable"))
+z=int(input("Enter value in third variable"))
+print("First variable is %d, Second variable is %d and Third variable is %d" %(x,y,z))
+
+#Question 4
+val='Let\'s get started'
+print(val)
+
+#Question 5
+s='Acadview'
+course='Python'
+fees=5000
+print("Name: %s , Course: %s , Fees: %d" %(s,course,fees))
+
+#Question 6
+name="Tony Stark"
+salary=1000000
+print('%s"%d'%(name,salary) )
